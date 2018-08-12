4e5953dcd900b450a3e998669af062687e79ae41
MsYqgzt <MsYqgzt_nick@163.com>
2018-08-13 00:18:55 +0800

    优化贴图显示，修改光照算法

--- TestingVersion/Adaptable Studio/obj/Debug/Minecraft Adaptable Studio_Content.g.i.cs
+++ TestingVersion/Adaptable Studio/obj/Debug/Minecraft Adaptable Studio_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icon/viewport3d/relocation.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("textures/masc/wood.png")]
 
 
